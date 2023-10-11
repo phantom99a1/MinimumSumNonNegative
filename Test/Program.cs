0d294df85ec0c505710e3e26544c555a0fa2de07
phantom99a1 <viendanho99a1@gmail.com>
2023-10-11 21:27:47 +0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Result
+    {
+        public static long number_of_bottle(long n,long k)
+        {
+            if (n < k) return n;
+
+            long emptyBottle = n % k;
+            long newBottle = n / k;
+            long numberBottle = n;
+            numberBottle += number_of_bottle(newBottle + emptyBottle, k)-emptyBottle;
+            return numberBottle;
+        }
+    }
+    public class Solution
+    {        
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            Console.WriteLine(Result.number_of_bottle(n , 3));
+            Console.ReadKey();
+        }
+    }
+}
